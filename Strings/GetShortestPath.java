3862e7f0a20e7b6e9805e051332c3873f8505d2e
afsarjahan ansari <nagma1804@gmail.com>
2023-03-16 15:52:04 +0530

    copying files from String branch

---
+++ Strings/GetShortestPath.java
@@ -0,0 +1,28 @@
+public class GetShortestPath {
+
+    public static float getShortestPath(String path){
+        int x=0, y=0;
+        for(int i=0; i<path.length(); i++){
+            char direction = path.charAt(i);
+            if(direction == 'S'){
+                y--;
+            }else if(direction == 'N'){
+                y++;
+            }else if(direction == 'W'){
+                x--;
+            }else {
+                x++;
+            }
+        }
+
+        int x2= x*x;
+        int y2=y*y;
+        return (float)Math.sqrt(x2+y2);
+    }
+
+
+    public static void main(String args[]){
+        String path = "NSWEWSNEWSW";
+        System.out.println("Shortest distance :" + getShortestPath(path));
+    }
+}
